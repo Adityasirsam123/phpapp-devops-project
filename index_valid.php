e0a995263da429b489c5a8e0bd2d912949d9abdb
Aditya  Sirsam <156391626+Adityasirsam123@users.noreply.github.com>
2025-05-10 19:57:22 +0530

    Update index_valid.php

--- index_valid.php
+++ index_valid.php
@@ -1,20 +1,15 @@
 <?php
 require_once 'db.php';
-if(isset($_POST['addtask'])){
-  $task_add = $_POST['textfield'];
-  if(!empty($task_add)){
-    $task_add_query = "INSERT INTO task_table (task_name) VALUES  ('$task_add')";
-    $add_query = $dbcon -> query($task_add_query);
-  }
 
-  header('location: index.php');
-  
+if (isset($_POST['addtask'])) {
+    $task_add = $_POST['textfield'];
+    if (!empty($task_add)) {
+        $task_add_query = "INSERT INTO task_table (task_name) VALUES  ('$task_add')";
+        $add_query = $dbcon->query($task_add_query);
+    }
 
-  
-
+    // Make sure this is called before any output
+    header('Location: index.php');
+    exit; // Stop script execution here
 }
-
-
-
-
 ?>
