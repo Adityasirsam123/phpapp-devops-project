266ce662905c1b66da65283401bc5003c52ebd79
Aditya  Sirsam <156391626+Adityasirsam123@users.noreply.github.com>
2025-05-10 19:56:18 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,11 @@
 <?php
-const HOST = 'localhost';
-const USERNAME = 'root';
-const PASSWORD = '';
+const HOST = '13.127.160.18'; // e.g., '172.31.XX.XX' or 'ec2-XX-XX-XX-XX.compute.amazonaws.com'
+const USERNAME = 'todo_user';
+const PASSWORD = 'admin';
 const DBNAME = 'todo';
+
 $dbcon = new mysqli(HOST, USERNAME, PASSWORD, DBNAME);
 
 if ($dbcon->connect_error) {
-  die("connect error: " . $dbcon->connect_error);
+    die("connect error: " . $dbcon->connect_error);
 }
-
-?>
